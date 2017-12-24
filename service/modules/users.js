4c6257690c1886c895b65ece8763f4b4b151b1e9
Enzo Martin <enzo.r.martin@gmail.com>
2017-12-24 19:35:18 +0100

    Populate APIs with actual logic - Reformat the routes to match method calls and callbacks - Ensure `next` is called consistently - Add some JSDoc

--- service/modules/users.js
+++ service/modules/users.js
@@ -1,27 +1,61 @@
+const uuid = require('uuid')
+
+const { getDbDate } = require('../utils')
 const { db } = require('../config')
 
 module.exports = {
+  /**
+   * Get all non-deleted users
+   * @param {Function} callback
+   * @returns {*}
+   */
   get: callback => {
-    db.query(
+    return db.query(
       'SELECT id, email FROM users WHERE deletedAt IS NULL ORDER BY email',
       callback
     )
   },
-  create: () => {
-    return [
-      {
-        id: 'new',
-        email: 'new@new.com',
-      },
-    ]
-  },
-  remove: () => {
-    return true
-  },
-  update: () => {
-    return {
-      id: 'id',
-      email: 'modified@new.com',
+  /**
+   * Add a new user
+   * @param {String} email
+   * @param {Function} callback
+   * @returns {*}
+   */
+  create: (email, callback) => {
+    const data = {
+      email,
+      id: uuid.v4(),
     }
+
+    return db.query('INSERT INTO users SET ?', data, callback)
+  },
+  /**
+   * Soft delete a user
+   * @param {String} id
+   * @param {Function} callback
+   * @returns {*}
+   */
+  remove: (id, callback) => {
+    const dateTime = getDbDate()
+
+    return db.query(
+      'UPDATE users SET updatedAt = ?, deletedAt = ? WHERE id = ?',
+      [dateTime, dateTime, id],
+      callback
+    )
+  },
+  /**
+   * Update an existing user
+   * @param {String} id
+   * @param {String} email
+   * @param {Function} callback
+   * @returns {*}
+   */
+  update: (id, email, callback) => {
+    return db.query(
+      'UPDATE users SET email = ?, updatedAt = ? WHERE id = ?',
+      [email, getDbDate(), id],
+      callback
+    )
   },
 }
