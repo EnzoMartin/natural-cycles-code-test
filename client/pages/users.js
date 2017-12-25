4a83f9a3f5bd482d414c38531d953396ebee201a
Enzo Martin <enzo.r.martin@gmail.com>
2017-12-25 23:44:20 +0100

    Hide CSS overflow

--- client/pages/users.js
+++ client/pages/users.js
@@ -98,6 +98,7 @@ export default class extends Component {
               min-width: 400px;
               margin: 0 8px;
               box-shadow: 0px 2px 2px 3px rgba(0, 0, 0, 0.2);
+              overflow: hidden;
             }
             table td,
             table th {
